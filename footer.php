697d8dcf74df3f796476ec9a204db25338d3d305
yaoshanliang <1329517386@qq.com>
2015-01-30 21:32:22 +0800

    add copyright sign

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <footer class="footer">
     <div class="footer-inner">
         <div class="copyright pull-left">
-Copyright 2013-2015  <a href="http://iat.net.cn" title="如果明天过后">如果明天过后</a> | Powered by <a href="http://wordpress.org" target="_blank">Wordpress</a> | Themed by <a href="http://yusi123.com" target="_blank">欲思博客</a> | Improved by <a href="https://github.com/yaoshanliang/iat_theme" target="_blank">iat_theme</a> | Safe by <a href='http://fuyun.safedog.cn' name='24d8e616791f3bebb69d3202b7a00434'  target='_blank'>安全狗</a> | Analysed by <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/iat.js"></script> 
+Copyright © 2013-2015  <a href="http://iat.net.cn" title="如果明天过后">如果明天过后</a> | Powered by <a href="http://wordpress.org" target="_blank">Wordpress</a> | Themed by <a href="http://yusi123.com" target="_blank">欲思博客</a> | Improved by <a href="https://github.com/yaoshanliang/iat_theme" target="_blank">iat_theme</a> | Safe by <a href='http://fuyun.safedog.cn' name='24d8e616791f3bebb69d3202b7a00434'  target='_blank'>安全狗</a> | Analysed by <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/iat.js"></script> 
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/iat.js"></script>
 
         </div>
