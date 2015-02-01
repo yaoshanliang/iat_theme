d7f1178b5af2d97fb5b84c6d7ee339a7da7ab3c7
yaoshanliang <1329517386@qq.com>
2015-02-01 14:59:48 +0800

    change 404 to lofter

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <div class="content-wrap">
 	<div style="text-align:center;padding:10px 0;font-size:16px;background-color:#ffffff;">
-		<h2 style="font-size:36px;margin-bottom:10px;">哎哟～404了～休息一下，去LOFTER上转转吧！</h2><script type="text/javascript">
+		<h2 style="font-size:36px;margin-bottom:10px;">哎哟～你居然发现了404！去<a href = "http://www.lofter.com" target = "_blank">LOFTER</a>上转转吧！</h2><script type="text/javascript">
 
 	</div>
 </div>
