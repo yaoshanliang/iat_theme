320b2a57f1c6670b20ea0f8df8658b5379cb5214
yaoshanliang <1329517386@qq.com>
2015-02-01 20:32:15 +0800

    add analysed by baidu

--- footer.php
+++ footer.php
@@ -4,8 +4,19 @@
 <footer class="footer">
     <div class="footer-inner">
         <div class="copyright pull-left">
-Copyright © 2013-2015  <a href="http://iat.net.cn" title="如果明天过后">如果明天过后</a> | Powered by <a href="http://wordpress.org" target="_blank">Wordpress</a> | Themed by <a href="http://yusi123.com" target="_blank">欲思博客</a> | Improved by <a href="https://github.com/yaoshanliang/iat_theme" target="_blank">iat_theme</a> | Safe by <a href='http://fuyun.safedog.cn' name='24d8e616791f3bebb69d3202b7a00434'  target='_blank'>安全狗</a> | Analysed by <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/iat.js"></script> 
-<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/iat.js"></script>
+Copyright © 2013-2015  <a href="http://iat.net.cn" title="如果明天过后">如果明天过后</a> | Powered by <a href="http://wordpress.org" target="_blank">Wordpress</a> | Themed by <a href="http://yusi123.com" target="_blank">欲思博客</a> | Improved by <a href="https://github.com/yaoshanliang/iat_theme" target="_blank">iat_theme</a> | Safe by <a href='http://fuyun.safedog.cn' name='24d8e616791f3bebb69d3202b7a00434'  target='_blank'>安全狗</a> | Analysed by <a href="http://tongji.baidu.com/web/7597801/trend/latest?siteId=5205380" target="_blank">百度统计</a>
+<!--统计代码开始-->
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?a8f517fee81bd1ee9a017d6f0cbfe789";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+<!--统计代码结束-->
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/iat.js"></script> 
 
         </div>
         <div class="trackcode pull-right">
